bc9927b7d34f9c6786836bb21df2457dafdea894
jacob273 <jakubgmur@op.pl>
2020-04-26 21:05:17 +0200

    1. removed unused utiltiies header 2. defined RGB_CHANNELS 3 3. Added overloaded GrayScaleCPU which does the gray scaling for rgbBmpArray 4. Added GetImgResolution(), GetTotalNumberOfPixels(), 5. fixed const height to be set properly (it was set to width) 6. Renamed BitmapToCharArray to RgbBitmapTo1dRgbCharArray 7. Renamed CharArrayToBitmap to OneDimensionCharArrayToRgbBitmap 8. OneDimensionCharArrayToRgbBitmap accepts now BMP object which all other data that are taken to be used for conversion 9. Replaced luminosity to avg calculation in GrayScaleImage__CudaKernel 10. Extended path building 11. Added checks and if image is not loaded properly, program stops execution 12. renamed a lot of variables to add a prefix d_ and h_ for them 13. changed the way block size is set (dim3 data structure is used) 14. Added memory freeing 15. added code which print out the values of avg array to verify if there are other values than memset is setting

--- JG.TechLearning.CUDA/lab2bmp.cu
+++ JG.TechLearning.CUDA/lab2bmp.cu
@@ -7,188 +7,246 @@
 #include <iomanip>
 #include <limits>
 #include <EasyBMP.h>
-#include <windows.h>
+#include <windows.h>    //to use GetCurrentDir
 #include <string.h>
-#include <EasyBMP_VariousBMPutilities.h>
 
 using namespace std::chrono;
 
-#define  BLOCK_SIZE 1024
+#define RGB_CHANNELS 3
 
 void GrayScaleCPU(BMP& originalBmp)
 {
-    for (int i = 0; i < originalBmp.TellWidth(); i++)
-    {
-        for (int j = 0; j < originalBmp.TellHeight(); j++)
-        {
+	for (int i = 0; i < originalBmp.TellWidth(); i++)
+	{
+		for (int j = 0; j < originalBmp.TellHeight(); j++)
+		{
 
-            double temporaryValue =
-                ( (originalBmp(i, j)->Red) +
-                  (originalBmp(i, j)->Green) +
-                  (originalBmp(i, j)->Blue)
-                ) / 3;
+			double temporaryValue =
+				((originalBmp(i, j)->Red) +
+				(originalBmp(i, j)->Green) +
+					(originalBmp(i, j)->Blue)
+					) / 3.0;
 
-            //Setting all three to avg
-            originalBmp(i, j)->Red = originalBmp(i, j)->Green = originalBmp(i, j)->Blue = (BYTE)temporaryValue;
-        }
-    }
+			//Setting all three to avg
+			originalBmp(i, j)->Red = originalBmp(i, j)->Green = originalBmp(i, j)->Blue = (ebmpBYTE)temporaryValue;
+		}
+	}
 }
 
-int GetImgTotalSize(BMP& img)
+BMP GrayScaleCPU(unsigned char* rgbBmpArray, BMP basedOn)
 {
-    int rgbChannels = 3;
-    return img.TellWidth() * img.TellHeight() * rgbChannels;
+	BMP newBmp = BMP(basedOn);
+
+	int width = newBmp.TellWidth();
+	int height = newBmp.TellHeight();
+
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+
+		double temporaryValue = (rgbBmpArray[(j * width) + i + 0]  +
+								 rgbBmpArray[(j * width) + i + 1]  +
+								 rgbBmpArray[(j * width) + i + 2]) 
+									/ 3.0;
+
+			//Setting all three to avg
+			newBmp(i, j)->Red = newBmp(i, j)->Green = newBmp(i, j)->Blue = (ebmpBYTE)temporaryValue;
+		}
+	}
+	return newBmp;
 }
 
-unsigned char* BitmapToCharArray(BMP& img)
+int GetImgResolution(BMP& img)
 {
-    int sizeOfArray = GetImgTotalSize(img);
-
-    unsigned char* output = new unsigned char[sizeOfArray];
-    memset(output, 0, sizeof(output));
-
-    int width = img.TellWidth();
-    int height = img.TellWidth();
-
-    for (int i = 0; i < width; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            output[(j * width) + i] = img(i, j)->Red;
-            output[(j * width) + i] = img(i, j)->Green;
-            output[(j * width) + i] = img(i, j)->Blue;
-        }
-    }
-    return output;
+	return img.TellWidth() * img.TellHeight();
 }
 
-BMP CharArrayToBitmap(unsigned char* img, int width, int height, int depth)
+int GetTotalNumberOfPixels(BMP& img)
 {
-    int rgbChannels = 3;
-    int sizeOfArray = width * height * rgbChannels;
-
-    BMP newBmp = BMP();
-    newBmp.SetSize(width, height);
-    newBmp.SetBitDepth(depth);
-
-    for (int i = 0; i < newBmp.TellWidth(); i++)
-    {
-        for (int j = 0; j < newBmp.TellHeight(); j++)
-        {
-            newBmp(i, j)->Red = img[(j * width) + i];
-            newBmp(i, j)->Green = img[(j * width) + i];
-            newBmp(i, j)->Blue = img[(j * width) + i];
-        }
-    }
-
-    return newBmp;
+	return GetImgResolution(img) * RGB_CHANNELS;
 }
 
-__global__ void GrayScaleImage__CudaKernel(unsigned char* in_Image, unsigned char* out_GrayImage, const int width, const int height)
+unsigned char* RgbBitmapTo1dRgbCharArray(BMP& img)
 {
-    int tx = (blockIdx.y * blockDim.y) + threadIdx.y;
-    int ty = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int sizeOfArray = GetTotalNumberOfPixels(img);
 
-    if ((ty < height && tx < width))
-    {
-        unsigned int pixelIndex = ty * width + tx;
-        float r = static_cast<float>(in_Image[pixelIndex * 3 + 0]);
-        float g = static_cast<float>(in_Image[pixelIndex * 3 + 1]);
-        float b = static_cast<float>(in_Image[pixelIndex * 3 + 2]);
+	unsigned char* output = new unsigned char[sizeOfArray];
+	memset(output, 0, sizeof(output));
 
-        float grayPix = (0.3f * r) + (0.59f * g) + (0.11f * b);
+	int width = img.TellWidth();
+	int height = img.TellHeight();
 
-        out_GrayImage[pixelIndex] = static_cast<unsigned char>(grayPix);
-    }
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			output[(j * width) + i + 0] = img(i, j)->Red;
+			output[(j * width) + i + 1] = img(i, j)->Green;
+			output[(j * width) + i + 2] = img(i, j)->Blue;
+		}
+	}
+
+	return output;
+}
+
+BMP OneDimensionCharArrayToRgbBitmap(unsigned char* img, BMP basedOn)
+{
+	int width = basedOn.TellWidth();
+	int height = basedOn.TellHeight();
+
+	BMP newBmp = BMP(basedOn);
+
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			newBmp(i, j)->Red = img[(j * width) + i];
+			newBmp(i, j)->Green = img[(j * width) + i];
+			newBmp(i, j)->Blue = img[(j * width) + i];
+		}
+	}
+	return newBmp;
+}
+
+__global__ void GrayScaleImage__CudaKernel(unsigned char* in_ImageRGB, unsigned char* out_GrayImage, const int width, const int height)
+{
+	int cols = width;
+	int rows = height;
+
+	//grid X
+	//block	    0,0
+	//thread	0,0	thread 1,0	thread 2,0 etc...
+
+	//block		1,0
+	//thread	0,0	thread	1,0	thread	2,0	etc...
+	int col = threadIdx.x + blockIdx.x * blockDim.x;
+	int row = threadIdx.y + blockIdx.y * blockDim.y;
+	
+	if (col < width && row < height)
+	{
+		int grey_offset = row * cols + col;
+
+		double r = in_ImageRGB[grey_offset + 0];
+		double g = in_ImageRGB[grey_offset + 1];
+		double b = in_ImageRGB[grey_offset + 2];
+
+		out_GrayImage[grey_offset] = double((r + g + b) / 3.0);
+	}
 }
 
 #define ErrorCheckCUDA(ans) { CheckErrorCUDA((ans), __FILE__, __LINE__); }
 inline void CheckErrorCUDA(cudaError_t code, const char* file, int line, bool abort = true)
 {
-    if (code != cudaSuccess)
-    {
-        fprintf(stderr, ":::::::::::::CheckErrorCUDA::::::::::::: %s %s %d\n", cudaGetErrorString(code), file, line);
-        if (abort) exit(code);
-    }
+	if (code != cudaSuccess)
+	{
+		fprintf(stderr, ":::::::::::::CheckErrorCUDA::::::::::::: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
+	}
 }
-
 
 int main(void)
 {
-    //Path preparation
-    char currentPathBuffer[256];
-    GetCurrentDirectoryA(256, currentPathBuffer);
-    std::string currentPathString = currentPathBuffer;
-    std::string fullFilePath = currentPathString.append("\\img\\example.bmp");
+	//Path preparation
+	char currentPathBuffer[256];
+	GetCurrentDirectoryA(256, currentPathBuffer);
+	std::string currentPathString = currentPathBuffer;
+	std::string fileName = "example.bmp";
+	std::string originalImageFullFilePath = currentPathString.append("\\img\\"+ fileName);
+	const int exampleNameOffset = fileName.length();
+	std::string cpuOutputPath = std::string(originalImageFullFilePath).insert(originalImageFullFilePath.length() - exampleNameOffset, "CPU-JakubGmur-Output-");
+	std::string gpuOutputPath = std::string(originalImageFullFilePath).insert(originalImageFullFilePath.length() - exampleNameOffset, "GPU-JakubGmur-Output-");
 
-    const int bitDepth = 16;
-    //Loading image for CPU
-	BMP bmpImageGrayScaledByCPU = BMP();
-	bmpImageGrayScaledByCPU.ReadFromFile(fullFilePath.c_str());
+	//Loading image for CPU
+	BMP sourceImageForCPU = BMP();
+	if (!sourceImageForCPU.ReadFromFile(originalImageFullFilePath.c_str()))
+	{
+		std::cout << "Could not load image... on a given path: " << originalImageFullFilePath << std::endl;
+		return -1;
+	}
 
-    std::cout << "Width of am image: " << bmpImageGrayScaledByCPU.TellWidth() << std::endl;
-    std::cout << "Height of am image: " << bmpImageGrayScaledByCPU.TellHeight() << std::endl;
-    std::cout << "Resolution (total number of pixels): " << bmpImageGrayScaledByCPU.TellWidth() * bmpImageGrayScaledByCPU.TellHeight() << std::endl;
-    std::cout << "Number of colors: " << bmpImageGrayScaledByCPU.TellNumberOfColors() << std::endl;
+	const int bitDepth = 16;
 
-    bmpImageGrayScaledByCPU.SetBitDepth(bitDepth);
-    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Started Gray Scaling Img on CPU " << std::endl;
-    GrayScaleCPU(bmpImageGrayScaledByCPU);
-    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Finished Gray Scaling Img on CPU" << std::endl;
-	bmpImageGrayScaledByCPU.WriteToFile(fullFilePath.insert(fullFilePath.length() - 4, "JakubGmur-Output-CPUCPU").c_str());
+	std::cout << "Width of an image: " << sourceImageForCPU.TellWidth() << std::endl;
+	std::cout << "Height of an image: " << sourceImageForCPU.TellHeight() << std::endl;
+	std::cout << "Resolution (total number of pixels): " << sourceImageForCPU.TellWidth() * sourceImageForCPU.TellHeight() << std::endl;
+	std::cout << "Number of colors: " << sourceImageForCPU.TellNumberOfColors() << std::endl;
 
-    //Conversion tests
-    //char* imageArr = BitmapToCharArray(bmpImageGrayScaledByCPU);
-    //BMP converted = CharArrayToBitmap(imageArr, bmpImageGrayScaledByCPU.TellWidth(), bmpImageGrayScaledByCPU.TellHeight(), bitDepth);
-    //converted.WriteToFile(fullFilePath.insert(fullFilePath.length() - 4, "JakubGmur-Output-Converted2222").c_str());
-    
-    //Loading image for GPU
-    BMP sourceImageForGPU = BMP();
-    sourceImageForGPU.ReadFromFile(fullFilePath.c_str());
-    sourceImageForGPU.SetBitDepth(bitDepth);
-    
-    //BMP conversion
-    unsigned char* hostImageArr = BitmapToCharArray(sourceImageForGPU);
-    const int totalSizeOfImg = GetImgTotalSize(sourceImageForGPU);
-    
-    //CUDA check
-    int  devCnt;
-    cudaGetDeviceCount(&devCnt);
-    if (devCnt == 0)
-    {
-        perror("No CUDA devices available -- exiting.");
-        return 1;
-    }
+	sourceImageForCPU.SetBitDepth(bitDepth);
+	//Gray scaling on CPU
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Started Gray Scaling Img on CPU " << std::endl;
+	GrayScaleCPU(sourceImageForCPU);
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Finished Gray Scaling Img on CPU" << std::endl;
+	if (!sourceImageForCPU.WriteToFile(cpuOutputPath.c_str()))
+	{
+		std::cout << "Could save image... on a given path: " << cpuOutputPath << std::endl;
+	}
 
-    //Allocation for CUDA
-    unsigned char* deviceImageArr;
-    ErrorCheckCUDA(cudaMalloc((void**)&deviceImageArr, totalSizeOfImg * sizeof(unsigned char)));
-    //Copying hostImageArr from Host to Device
-    ErrorCheckCUDA(cudaMemcpy(deviceImageArr, hostImageArr, totalSizeOfImg * sizeof(unsigned char), cudaMemcpyHostToDevice));
+	//Loading image for GPU
+	BMP sourceImageForGPU = BMP();
+	if (!sourceImageForGPU.ReadFromFile(originalImageFullFilePath.c_str()))
+	{
+		std::cout << "Could not load image... on a given path: " << originalImageFullFilePath << std::endl;
+	}
 
-    // Block size calculation
-    int imageSize = sourceImageForGPU.TellWidth() * sourceImageForGPU.TellHeight();
-    int rgbChannels = 3;
+	sourceImageForGPU.SetBitDepth(bitDepth);
 
-    int totalNumberOfBlocksToUse = 1024;
-    int blocks = (sourceImageForGPU.TellWidth() * sourceImageForGPU.TellHeight()) / totalNumberOfBlocksToUse;
-    if (imageSize % totalNumberOfBlocksToUse)
-    {
-        blocks++;
-    }
+	//BMP conversion
+	unsigned char* h_rgbImageArr = RgbBitmapTo1dRgbCharArray(sourceImageForGPU); 
+	
+	//reversed conversion test
+	//BMP bmpresult1 = GrayScaleCPU(h_rgbImageArr, sourceImageForGPU);
+	//bmpresult1.WriteToFile((gpuOutputPath).c_str());
+	//return 0;
 
-    //Obliczanie wartosci sredniej piksela
-    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Started Gray Scaling Img on GPU " << std::endl;
-    GrayScaleImage__CudaKernel << <blocks, totalNumberOfBlocksToUse>> > (hostImageArr, deviceImageArr, sourceImageForGPU.TellWidth(), sourceImageForGPU.TellHeight());
-    ErrorCheckCUDA(cudaThreadSynchronize());
-    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Finished Gray Scaling Img on GPU" << std::endl;
+	//CUDA device check
+	int  devCnt;
+	cudaGetDeviceCount(&devCnt);
+	if (devCnt == 0)
+	{
+		std::cout << "No CUDA devices available -- exiting." << std::endl;
+		return -1;
+	}
 
+	//Allocation for CUDA
+	const int imageResolution = GetImgResolution(sourceImageForGPU);
+	unsigned char* d_rgbInputImageArr_cuda;      //array for storing RGB data on cuda device
+	unsigned char* d_AvgOfRgbImgArr_cuda;        //array for storing (avg of RGB) data on cuda device
 
-    //Copying deviceImageArr from Device to Host
-    const int graySize = sourceImageForGPU.TellWidth() * sourceImageForGPU.TellHeight();
-    ErrorCheckCUDA(cudaMemcpy(hostImageArr, deviceImageArr, graySize * sizeof(unsigned char), cudaMemcpyDeviceToHost));
-    ErrorCheckCUDA(cudaFree(deviceImageArr));
-    BMP gpuResult = CharArrayToBitmap(hostImageArr, sourceImageForGPU.TellWidth(), sourceImageForGPU.TellHeight(), bitDepth);
-    gpuResult.WriteToFile(fullFilePath.insert(fullFilePath.length() - 4, "JakubGmur-Output-GPUGPU").c_str());
+	ErrorCheckCUDA(cudaMalloc((void**)&d_rgbInputImageArr_cuda, imageResolution * RGB_CHANNELS * sizeof(unsigned char)));
+	ErrorCheckCUDA(cudaMalloc((void**)&d_AvgOfRgbImgArr_cuda, imageResolution * sizeof(unsigned char)));
+	cudaMemset(d_AvgOfRgbImgArr_cuda, 255, imageResolution * sizeof(unsigned char));//setting all pixels to be gray ~222
+	ErrorCheckCUDA(cudaMemcpy(d_rgbInputImageArr_cuda, h_rgbImageArr, imageResolution * sizeof(unsigned char), cudaMemcpyHostToDevice));
+
+	// Block size
+	dim3 dimBlock(32, 32);
+	dim3 dimGrid(ceil((sourceImageForGPU.TellWidth() - 1) / dimBlock.x), ceil((sourceImageForGPU.TellHeight() + dimBlock.y - 1) / dimBlock.y));
+	
+	//Gray scaling on GPU
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Started Gray Scaling Img on GPU " << std::endl;
+	GrayScaleImage__CudaKernel << <dimGrid, dimBlock >> > (d_rgbInputImageArr_cuda, d_AvgOfRgbImgArr_cuda, sourceImageForGPU.TellWidth(), sourceImageForGPU.TellHeight());
+	ErrorCheckCUDA(cudaThreadSynchronize());
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~Finished Gray Scaling Img on GPU" << std::endl;
+
+	//Copying from Device to Host
+	unsigned char* h_AvgOfRgbImgArr = new unsigned char[imageResolution];  //array for storing (avg of RGB) data on local device
+	ErrorCheckCUDA(cudaMemcpy(h_AvgOfRgbImgArr, d_AvgOfRgbImgArr_cuda, imageResolution * sizeof(unsigned char), cudaMemcpyDeviceToHost));
+
+	for (int i = 0; i < 1000; i++)
+	{
+		std::cout << (int)h_AvgOfRgbImgArr[i] << std::endl;
+	}
+
+	//Writing 
+	BMP gpuResult = OneDimensionCharArrayToRgbBitmap(h_AvgOfRgbImgArr, sourceImageForGPU);
+	if (!gpuResult.WriteToFile(gpuOutputPath.c_str()))
+	{
+		std::cout << "Could save image... on a given path: " << cpuOutputPath << std::endl;
+	}
+
+	ErrorCheckCUDA(cudaFree(d_AvgOfRgbImgArr_cuda));
+	ErrorCheckCUDA(cudaFree(d_rgbInputImageArr_cuda));
+	free(h_rgbImageArr);
+	free(h_AvgOfRgbImgArr);
 	return 0;
 }
